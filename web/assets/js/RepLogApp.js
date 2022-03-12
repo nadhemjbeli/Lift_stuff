33be8fd7c83a2061b6621ff42a276ebb1f461cd3
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-12 18:52:28 +0100

    javascript - instanciate objects & constructors

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -4,7 +4,7 @@
     window.RepLogApp ={
         initialize: function($wrapper){
             this.$wrapper = $wrapper;
-            Helper.initialize($wrapper);
+            this.helper = new Helper($wrapper);
 
             this.$wrapper.find('.js-delete-rep-log').on(
                 'click',
@@ -23,7 +23,7 @@
 
         updateTotalRepLogDelete: function (){
             this.$wrapper.find('.js-total-weight').html(
-                Helper.calculateTotalWeight()
+                this.helper.calculateTotalWeight()
             );
         },
         handleRepLogDelete: function (e){
@@ -60,16 +60,15 @@
     /**
      * A "private" object
      */
-    var Helper ={
-        initialize: function($wrapper) {
+    var Helper = function($wrapper) {
             this.$wrapper = $wrapper;
-        },
-        calculateTotalWeight: function (){
+        };
+        Helper.calculateTotalWeight = function (){
             var totalWeight = 0;
             this.$wrapper.find('tbody tr').each(function (){
                 totalWeight+=$(this).data('weight');
             })
             return totalWeight;
         }
-    }
+
 })(window, jQuery);
