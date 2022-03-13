31692989527fe93b6e3e07e3a4b89b36901571d0
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-13 17:16:55 +0100

    ES6 - Map

--- play.js
+++ play.js
@@ -1,6 +1,19 @@
-let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
-let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
-let copyOfGreatThings = [...greatThings];
-copyOfGreatThings.push('summer');
-console.log(greatThings);
-console.log(copyOfGreatThings);
+// let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
+// let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
+// let copyOfGreatThings = [...greatThings];
+// copyOfGreatThings.push('summer');
+// console.log(greatThings);
+// console.log(copyOfGreatThings);
+
+let foods = new Map();
+foods.set('italian', 'gelato');
+foods.set('mexican', 'tortas');
+foods.set('canadian', 'poutine');
+
+let southernUsStates = ['Tennessee', 'Kentucky', 'Texas'];
+foods.set(southernUsStates, 'hot chicken');
+
+console.log(
+    foods.get('italian'),
+    foods.get(southernUsStates)
+);
